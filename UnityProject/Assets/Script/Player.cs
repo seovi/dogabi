0f9c334994fb897c55f84a8c29f3faa305a93028
seovi <sube72@gmail.com>
2019-01-15 10:12:34 +0900

    Update Player.cs
    Commit test

--- UnityProject/Assets/Script/Player.cs
+++ UnityProject/Assets/Script/Player.cs
@@ -18,16 +18,16 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        //transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
 		if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
-            transform.position = targetPos;
+            //transform.position = targetPos;
         } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
-            transform.position = targetPos;
+            //transform.position = targetPos;
         }
 	}
 }
