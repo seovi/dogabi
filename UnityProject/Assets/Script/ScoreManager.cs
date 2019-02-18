b53b140ef3a5007bc483c9a468e71997ddae29e3
Lim Byung Jik <h2omit@gmail.com>
2019-02-19 01:26:46 +0900

    스코어 추가
    스코어 추가 할려고 하는데 텍스트가 현재 찍히지 않음

---
+++ UnityProject/Assets/Script/ScoreManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            ++score;
+            Debug.Log(score);
+        }
+    }
+}
